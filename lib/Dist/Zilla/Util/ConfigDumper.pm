a0c2803ff9d53c29d9121bfdd166e6baaa7bb098
Kent Fredric <kentfredric@gmail.com>
2014-08-23 05:53:06 +1200

    Build results of 8e46997 (on master)

--- lib/Dist/Zilla/Util/ConfigDumper.pm
+++ lib/Dist/Zilla/Util/ConfigDumper.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::ConfigDumper;
 
-our $VERSION = '0.002000';
+our $VERSION = '0.002001';
 
 # ABSTRACT: Easy implementation of 'dumpconfig'
 
@@ -99,7 +99,7 @@ Dist::Zilla::Util::ConfigDumper - Easy implementation of 'dumpconfig'
 
 =head1 VERSION
 
-version 0.002000
+version 0.002001
 
 =head1 SYNOPSIS
 
