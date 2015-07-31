de668bac94aedc7c14433a213d76d68decdaa33b
Kent Fredric <kentfredric@gmail.com>
2015-07-31 15:56:28 +1200

    v0.003007
     [Dependencies::Stats]
     - Dependencies changed since 0.003006, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: +12 ↑3 -3 (recommends: +1, suggests: ↑2)
     - runtime: ↓1 -1
     - test: +1 (recommends: +1 ↑1 ↓1)
    
     [Documentation]
     - Enhance DESCRIPTION section and ABSTRACT as per request #3

--- lib/Dist/Zilla/Util/ConfigDumper.pm
+++ lib/Dist/Zilla/Util/ConfigDumper.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::ConfigDumper;
 
-our $VERSION = '0.003007';
+our $VERSION = '0.003008';
 
 # ABSTRACT: A Dist::Zilla plugin configuration extraction utility
 
