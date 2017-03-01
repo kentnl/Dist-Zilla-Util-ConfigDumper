5e11d48681d4e6f8a3e696a91e9df9d3f2bf67bf
Kent Fredric <kentfredric@gmail.com>
2017-03-02 11:42:45 +1300

    Build results of 33a0d16 (on master)

--- lib/Dist/Zilla/Util/ConfigDumper.pm
+++ lib/Dist/Zilla/Util/ConfigDumper.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::ConfigDumper;
 
-our $VERSION = '0.003008';
+our $VERSION = '0.003009';
 
 # ABSTRACT: A Dist::Zilla plugin configuration extraction utility
 
@@ -196,7 +196,7 @@ Dist::Zilla::Util::ConfigDumper - A Dist::Zilla plugin configuration extraction 
 
 =head1 VERSION
 
-version 0.003008
+version 0.003009
 
 =head1 SYNOPSIS
 
